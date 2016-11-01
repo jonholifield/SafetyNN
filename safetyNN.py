517d15ba461914157c0e61b0fdb5dc2198e29930
Jonathan Holifield <jonholifield@gmail.com>
2016-10-31 20:22:14 -0400

    Environment Training
    Added a means and NN to learn the Environment that it is in so that it
    can guess reactions to it.

--- safetyNN.py
+++ safetyNN.py
@@ -13,7 +13,7 @@ H2 = 30
 E1 = 100
 E2 = 100
 E3 = 100
-batch_number = 50
+batch_number = 20
 gamma = 0.99
 explore = 1
 num_of_episodes_between_q_copies = 100
@@ -32,31 +32,31 @@ if __name__ == '__main__':
     tf.reset_default_graph()
 
     #Enviroment Model
-    Env_weights1 = tf.Variable(tf.random_uniform([6, E1], .1, 1.0)
+    Env_weights1 = tf.Variable(tf.random_uniform([5, E1], .1, 1.0))
     Env_bias1 = tf.Variable(tf.random_uniform([E1], .1, 1.0))
     
-    Env_weights2 = tf.Variable(tf.random_uniform([E1, E2], .1, 1.0)
+    Env_weights2 = tf.Variable(tf.random_uniform([E1, E2], .1, 1.0))
     Env_bias2 = tf.Variable(tf.random_uniform([E2], .1, 1.0))
     
-    Env_weights3 = tf.Variable(tf.random_uniform([E2, E3], .1, 1.0)
+    Env_weights3 = tf.Variable(tf.random_uniform([E2, E3], .1, 1.0))
     Env_bias3 = tf.Variable(tf.random_uniform([E3], .1, 1.0))
     
-    Env_weights4 = tf.Variable(tf.random_uniform([E3, 4], .1, 1.0)
-    Env_bias3 = tf.Variable(tf.random_uniform([4], .1, 1.0))
+    Env_weights4 = tf.Variable(tf.random_uniform([E3, 4], .1, 1.0))
+    Env_bias4 = tf.Variable(tf.random_uniform([4], .1, 1.0))
     
     #Envirment Model Map
-    states_with_action = tf.placeholder(tf.float32, [None, 6], name="states_with_actions")  
+    states_with_action = tf.placeholder(tf.float32, [None, 5], name="states_with_actions")  
     Env_hidden_1 = tf.nn.relu(tf.matmul(states_with_action, Env_weights1) + Env_bias1)
     Env_hidden_2 = tf.nn.relu(tf.matmul(Env_hidden_1, Env_weights2) + Env_bias2)
     Env_hidden_3 = tf.nn.relu(tf.matmul(Env_hidden_2, Env_weights3) + Env_bias3)
-    next_states_guess = tf.matmul(Env_hidden_3, Env_weights4) + Env_bias3
+    next_states_guess = tf.matmul(Env_hidden_3, Env_weights4) + Env_bias4
     
     real_states = tf.placeholder(tf.float32, [None, 4], name="real_states_env")
     Env_loss = tf.reduce_mean(tf.square(real_states - next_states_guess))
     Env_train = tf.train.AdamOptimizer(learning_rate).minimize(Env_loss) 
     
     #First Q Network
-    w1 = tf.Variable(tf.random_uniform([env.observation_space.shape[0],H], .1, 1.0))
+    w1 = tf.Variable(tf.random_uniform([4,H], .1, 1.0))
     bias1 = tf.Variable(tf.random_uniform([H], .1, 1.0))
     
     w2 = tf.Variable(tf.random_uniform([H,H2], .1, 1.0))
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     w3 = tf.Variable(tf.random_uniform([H2,env.action_space.n], .1, 1.0))
     bias3 = tf.Variable(tf.random_uniform([env.action_space.n], .1, 1.0))
     
-    states = tf.placeholder(tf.float32, [None, env.observation_space.shape[0]], name="states")  # This is the list of matrixes that hold all observations
+    states = tf.placeholder(tf.float32, [None, 4], name="states")  # This is the list of matrixes that hold all observations
     #actions = tf.placeholder(tf.float32, [None, env.action_space.n], name="actions")
     
     hidden_1 = tf.nn.relu(tf.matmul(states, w1) + bias1)
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     one_hot_actions = tf.one_hot(actions, env.action_space.n)
     Q = tf.reduce_sum(tf.mul(action_values, one_hot_actions), reduction_indices=1) 
 
-    w1_prime = tf.Variable(tf.random_uniform([env.observation_space.shape[0],H], .1, 1.0))
+    w1_prime = tf.Variable(tf.random_uniform([4,H], .1, 1.0))
     bias1_prime = tf.Variable(tf.random_uniform([H], .1, 1.0))
     
     w2_prime = tf.Variable(tf.random_uniform([H,H2], .1, 1.0))
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     
     #Second Q network
     
-    next_states = tf.placeholder(tf.float32, [None, env.observation_space.shape[0]], name="n_s") # This is the list of matrixes that hold all observations
+    next_states = tf.placeholder(tf.float32, [None, 4], name="n_s") # This is the list of matrixes that hold all observations
     hidden_1_prime = tf.nn.relu(tf.matmul(next_states, w1_prime) + bias1_prime)
     hidden_2_prime = tf.nn.relu(tf.matmul(hidden_1_prime, w2_prime) + bias2_prime)
     next_action_values =  tf.matmul(hidden_2_prime, w3_prime) + bias3_prime
@@ -138,7 +138,8 @@ if __name__ == '__main__':
         for episode in xrange(max_episodes):
             print 'Reward for episode %f is %f. Explore is %f' %(episode,reward_sum, explore)
             reward_sum = 0
-            new_state = env.reset()
+            new_state_big  = env.reset()
+            (shutdown, new_state) = new_state_big 
             
             for step in xrange(max_steps):
                 
@@ -154,15 +155,20 @@ if __name__ == '__main__':
                     results = sess.run(action_values, feed_dict={states: np.array([new_state])})
                     #print results
                     action = (np.argmax(results[0]))
-
-                    
-                curr_action = action;
                 
-                observedTuple = env.step(action)
-                new_state, reward, done, _ = observedTuple[1]
+                curr_state_with_action = []
+                curr_state_with_action.append([state[0],state[1],state[2],state[3],action])
+                new_state_guess = sess.run(next_states_guess, feed_dict={states_with_action: curr_state_with_action})    
+                curr_action = action;
+                whatAmI = env.step(action)
+                #print whatAmI
+                new_state_big, reward, done, _  = whatAmI
+                (shutdown, new_state) = new_state_big
+                #print new_state
                 reward_sum += reward
                 
-                
+                if episode % batch_number == 0:
+                    print "State:", new_state, " Guess:", new_state_guess
                 
                 D.append([state, curr_action, reward, new_state, done])
                 
@@ -183,6 +189,7 @@ if __name__ == '__main__':
                     new_states_for_q = [ x[3] for x in samples]
                     all_q_prime = sess.run(next_action_values, feed_dict={next_states: new_states_for_q})
                     y_ = []
+                    Env_input_states = []
                     states_samples = []
                     next_states_samples = []
                     actions_samples = []
@@ -200,12 +207,18 @@ if __name__ == '__main__':
                             #print y_
                         #y_.append(i_sample[2])
                         states_samples.append(i_sample[0])
+                        #print i_sample[0][0]
+                        #print i_sample[0][1]
+                        #print i_sample[0][2]
+                        #print i_sample[0][3]
+                        #print i_sample[1]
+                        Env_input_states.append([i_sample[0][0], i_sample[0][1], i_sample[0][2], i_sample[0][3], i_sample[1]])
                         next_states_samples.append(i_sample[3])
                         actions_samples.append(i_sample[1])
                     
                     #print sess.run(loss, feed_dict={states: states_samples, next_states: next_states_samples, rewards: y_, actions: actions_samples})#feed_dict={states: states_samples, next_states: next_states_samples, rewards: y_, actions: actions_samples, one_hot: actions_samples})
                     sess.run(train, feed_dict={states: states_samples, next_states: next_states_samples, rewards: y_, actions: actions_samples})
-                    sess.run(Env_train, feed_dict={states: states_samples, next_states: next_states_samples})
+                    sess.run(Env_train, feed_dict={states_with_action: Env_input_states, real_states: next_states_samples})
                     #y_ = reward + gamma * sess.run(next_action_values, feed_dict={next_states: np.array([i_sample[3]])})
                     #y_ = curr_action * np.vstack([y_])
                     #print y_
